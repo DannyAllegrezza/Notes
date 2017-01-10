69495d5f9b5887a3faa93528910e40f4abac2c1a
Danny Allegrezza <danny@dannyallegrezza.com>
2017-01-10 01:00:33 -0500

    Mock auth and registration
    * Add fade transistion to Auth, nice touch. Use the <transition>
    attribute to do so -->
    https://vuejs.org/v2/guide/transitions.html#Transition-Classes
    
    * Register.vue: Add a stubbed out model for the registration form, add
    method to register user. Currently just logs the user object.

--- Vue.js/beeper-client/src/components/auth/Register.vue
+++ Vue.js/beeper-client/src/components/auth/Register.vue
@@ -1,12 +1,12 @@
 <template>
 	<div>
 		<h3 class="text-center">Register</h3>
-		<input type="email" class="form-control m-b-15" placeholder="Email address">
-		<input type="text" class="form-control m-b-15" placeholder="Username">
-		<input type="password" class="form-control m-b-15" placeholder="Password">
+		<input type="email" class="form-control m-b-15" placeholder="Email address" v-model="user.email">
+		<input type="text" class="form-control m-b-15" placeholder="Username" v-model="user.username">
+		<input type="password" class="form-control m-b-15" placeholder="Password" v-model="user.password">
 		<hr>
 
-		<button class="btn btn-lg btn-primary btn-block m-b-15">Register</button>
+		<button class="btn btn-lg btn-primary btn-block m-b-15" @click="register">Register</button>
 
 		<p class="text-center">
 			Already have an account? <router-link to="/auth/login">Login!</router-link>
@@ -15,7 +15,21 @@
 </template>
 <script>
 	export default {
-		name: "register"
+		name: "register",
+		data: function() {
+			return {
+				user: {
+					email: "",
+					username: "",
+					password: "",
+				}
+			};
+		},
+		methods: {
+			register: function (){
+				console.log(this.user);
+			}
+		}
 	}
 
 </script>
